9d418732f497818783978948074cb4dce4742208
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2023-05-11 16:29:16 -0400

    Revert "build your own react exercise (broken)"
    This reverts commit 725cb816a30ab1edb3c1d928edd3d8214098399d. To revert broken exercise and continue.
    
    Revert changes

--- build-your-own-react/src/index.js
+++ build-your-own-react/src/index.js
@@ -1,68 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child =>
-        typeof child === 'object' ? child : createTextElement(child)  
-      ),
-    },
-  }
-}
-
-function createTextElement(text) {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: [],
-    }
-  }
-}
-
-
-/** @jsx Didact.createElement */
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
-)
-
-
-const container = document.getElementById('root');
-
-function render(element, container) {
-  // TODO create dom nodes
-  const dom = 
-    element.type === 'TEXT_ELEMENT'
-      ? document.createTextNode('')
-      : document.createElement(element.type);
-
-  const isProperty = key => key !== "children"
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name]
-    })
-
-  element.props.children.forEach(child => {
-    render(child, dom);
-  });
-
-
-  container.appendChild(dom);
-
-}
-
-const Didact = {
-  createElement,
-  render,
-}
-
-Didact.render(element, container);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
